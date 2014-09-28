bc70c49c32ede78778fb2a51afdbfdc07e48bd63
Fernando <rodriguesaguirre@gmail.com>
2014-09-28 18:14:49 -0300

    modificado: scripts/app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@ require.config({
   },
   shim: {
     'three': { exports: 'THREE' },
-    'controls/OrbitControls': { deps: [ 'three' ] },
+    'OrbitControls': { deps: [ 'three' ] },
     'dat.gui.min': { exports: 'dat.GUI' }
   }
 });
@@ -30,6 +30,7 @@ requirejs(['Stats', 'Detector', 'OrbitControls', 'three'],
 */
 
 // MAIN
+//<div id="ThreeJS" style="position: absolute; left:0px; top:0px"></div>
 
 var container, scene, camera, renderer, controls, stats;
 
@@ -40,146 +41,140 @@ var parameters;
 var gui;
 
 init();
-animate();
+//animate();
 
 // FUNCTIONS 		
 function init() 
 {
 	// SCENE
-	require(['three'], function(THREE) 
+	require(['three','OrbitControls','Stats','underscore'], function() 
 	{
 		var scene = new THREE.Scene();
-	});
-	//scene = new THREE.Scene();
-	// CAMERA
-	var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	scene.add(camera);
-	camera.position.set(0,150,400);
-	camera.lookAt(scene.position);	
+		//scene = new THREE.Scene();
+		// CAMERA
+		var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+		var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+		camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+		scene.add(camera);
+		camera.position.set(0,150,400);
+		camera.lookAt(scene.position);	
 	// RENDERER
-	require(['three'], function(THREE) 
-	{
-		var renderer = new THREE.WebGLRenderer( {antialias:true} );
-	});
-	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	container = document.getElementById( 'ThreeJS' );
-	container.appendChild( renderer.domElement );
-	// EVENTS
-
+	//<div id="ThreeJS" style="position: absolute; left:0px; top:0px"></div>
+		renderer = new THREE.WebGLRenderer( {antialias:true} );
+		renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		//container.domElement.position = 'absolute';
+		//container.domElement.left = '0px';
+		//container.domElement.top = '0px';
+		container = document.getElementById( "ThreeJS" );
+		container.appendChild( renderer.domElement ); //add a new child node
 	// CONTROLS
-	require(['three'], function(THREE) 
+		var controls = new THREE.OrbitControls( camera, renderer.domElement ); //entra objeto e dom.element
+	// STATS
+		stats = new Stats();
+		stats.domElement.style.position = 'absolute';
+		stats.domElement.style.bottom = '0px';
+		stats.domElement.style.zIndex = 100;
+		container.appendChild( stats.domElement );
+	//Sphere
+		var sphereGeometry = new THREE.SphereGeometry( 50, 50, 50 );
+		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
+		sphere = new THREE.Mesh( sphereGeometry , sphereMaterial );
+		scene.add(sphere);
+	});
+	
+	require(['dat.gui.min'], function(GUI) 
 	{
-		var controls = new THREE.OrbitControls( camera, renderer.domElement );
+		gui = new dat.GUI();
+	
+		parameters = 
+		{
+			radius:50,
+			x:aleatorio(-100, 100) , y:aleatorio(-100, 100), z:aleatorio(-100, 100),
+			visible: false,	
+			rand: function() { randSphere() },
+			creat: function() { creatSphere() },
+			reset: function() { resetAll() }
+		};
+
+		var sphereRadius = gui.add(parameters,'radius').min(0).max(300).step(1).listen();
+		sphereRadius.onChange(function(value)
+		{	sphere.radius = value;		});
+
+		var sphereVisible = gui.add( parameters, 'visible' ).name('Visible?').listen();
+		sphereVisible.onChange(function(value) 
+		{   sphere.visible = value;  	});
+
+
+		gui.add( parameters, 'rand' ).name("rand Sphere");
+
+		gui.add( parameters, 'creat' ).name("creat Sphere");
+
+		gui.add( parameters, 'reset' ).name("reset");
+	
+		gui.open();
+		updateSphere();
 	});
 
-	// STATS
-	stats = new Stats();
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.bottom = '0px';
-	stats.domElement.style.zIndex = 100;
-	container.appendChild( stats.domElement );
-
-	//Sphere
-	var sphereGeometry = new THREE.SphereGeometry( 50, 50, 50 );
-	var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
-	sphere = new THREE.Mesh( sphereGeometry , sphereMaterial );
-	scene.add(sphere);
-
-	
-	gui = new dat.GUI();
-	
-	parameters = 
+	function aleatorio(min, max) 
 	{
-		radius:50,
-		x:aleatorio(-100, 100) , y:aleatorio(-100, 100), z:aleatorio(-100, 100),
-		visible: false,	
-		rand: function() { randSphere() },
-		creat: function() { creatSphere() },
-		reset: function() { resetAll() }
-	};
+		return Math.random() * (max - min) + min;
+	}
 
-	var sphereRadius = gui.add(parameters,'radius').min(0).max(300).step(1).listen();
-	sphereRadius.onChange(function(value)
-	{	sphere.radius = value;		});
+	function randSphere()
+	{
+		parameters.x = aleatorio(-200, 200);
+		parameters.y = aleatorio(-200, 200);
+		parameters.z = aleatorio(-200, 200);
+		updateSphere();
+	}
 
-	var sphereVisible = gui.add( parameters, 'visible' ).name('Visible?').listen();
-	sphereVisible.onChange(function(value) 
-	{   sphere.visible = value;  	});
+	function creatSphere()
+	{
+		sphere_Geometry = new THREE.SphereGeometry( parameters.radius, 50, 50 );
+		sphere_Material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
+		sphere_new = new THREE.Mesh( sphere_Geometry , sphere_Material );
+		sphere_new.position.x = parameters.x;
+		sphere_new.position.y = parameters.y;
+		sphere_new.position.z = parameters.z;
+		scene.add(sphere_new);
+	}
 
+	function resetAll()
+	{
+		var objsToRemove = _.rest(scene.children, 1);
+	    _.each(objsToRemove, function( sphere, sphere_new ) {
+    	      scene.remove(sphere, sphere_new);
+    	});
+	    var sphereGeometry = new THREE.SphereGeometry( 50, 50, 50 );
+		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
+		sphere = new THREE.Mesh( sphereGeometry , sphereMaterial );
+		scene.add(sphere);
+	}
 
-	gui.add( parameters, 'rand' ).name("rand Sphere");
-
-	gui.add( parameters, 'creat' ).name("creat Sphere");
-
-	gui.add( parameters, 'reset' ).name("reset");
-	
-	gui.open();
-	updateSphere();
-	
-}
-
-
-function aleatorio(min, max) 
-{
-	return Math.random() * (max - min) + min;
-}
-
-function randSphere()
-{
-	parameters.x = aleatorio(-200, 200);
-	parameters.y = aleatorio(-200, 200);
-	parameters.z = aleatorio(-200, 200);
-	updateSphere();
-}
-
-function creatSphere()
-{
-	sphere_Geometry = new THREE.SphereGeometry( parameters.radius, 50, 50 );
-	sphere_Material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
-	sphere_new = new THREE.Mesh( sphere_Geometry , sphere_Material );
-	sphere_new.position.x = parameters.x;
-	sphere_new.position.y = parameters.y;
-	sphere_new.position.z = parameters.z;
-	scene.add(sphere_new);
-}
-
-function resetAll()
-{
-	var objsToRemove = _.rest(scene.children, 1);
-    _.each(objsToRemove, function( sphere, sphere_new ) {
-          scene.remove(sphere, sphere_new);
-    });
-    var sphereGeometry = new THREE.SphereGeometry( 50, 50, 50 );
-	var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
-	sphere = new THREE.Mesh( sphereGeometry , sphereMaterial );
-	scene.add(sphere);
-}
-
-function updateSphere()
-{
-	//sphereGeometry = new THREE.SphereGeometry( parameters.radius, 50, 50 )
-	sphere.position.x = parameters.x;
-	sphere.position.y = parameters.y;
-	sphere.position.z = parameters.z;
-	sphere.visible = parameters.visible;
-}
+	function updateSphere()
+	{
+		//sphereGeometry = new THREE.SphereGeometry( parameters.radius, 50, 50 )
+		sphere.position.x = parameters.x;
+		sphere.position.y = parameters.y;
+		sphere.position.z = parameters.z;
+		sphere.visible = parameters.visible;
+	}
 
 
-function animate() 
-{
-    requestAnimationFrame( animate );
-	render();		
-	update();
-}
+	function animate() 
+	{
+   		requestAnimationFrame( animate );
+		render();		
+		update();
+	}
 
-function update()
-{	
-	controls.update();
-	stats.update();
-}
-function render() 
-{
-	renderer.render( scene, camera );
+	function update()
+	{	
+		controls.update();
+		stats.update();
+	}
+	function render() 
+	{
+		renderer.render( scene, camera );
+	}
 }
